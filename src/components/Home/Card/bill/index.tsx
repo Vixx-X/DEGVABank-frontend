08eadac76d3cf3b212909985bfdaab6a11589ce3
Gabriela Ustariz <gabrielaustariz@gmail.com>
2022-02-10 06:52:51 -0400

    fixed errors in home page

--- src/components/Home/Card/bill/index.tsx
+++ src/components/Home/Card/bill/index.tsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-interface Bill{
-  id:number,
-  status:string,
-  type:string,
-  balance:string,
-  creation_date:string,
-  user:number
+interface Bill {
+  id: number;
+  status: string;
+  type: string;
+  balance: string;
+  creation_date: string;
+  user: number;
 }
-interface CardBillProps{
-  ITEMS_BILLS: Bill[]
+interface CardBillProps {
+  ITEMS_BILLS: Bill[];
 }
 
-const CardBill = ({ITEMS_BILLS}:CardBillProps) => {
-  const [bill, setbill] = useState(ITEMS_BILLS[0]);
+const CardBill = ({ ITEMS_BILLS }: CardBillProps) => {
+  const [bill, setbill] = useState<any>();
 
   const handleCurrentBill = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const findBill = ITEMS_BILLS.find(({ id }) => e.target.value == id.toString());
+    const findBill = ITEMS_BILLS.find(
+      ({ id }) => e.target.value == id.toString()
+    );
     if (findBill) {
       setbill(findBill);
     }
   };
+
+  useEffect(() => {
+    if (ITEMS_BILLS) {
+      setbill(ITEMS_BILLS[0]);
+    }
+  }, [ITEMS_BILLS]);
+
   return (
     <>
       <div className="rounded-2xl w-full overflow-hidden shadow-lg p-8">
@@ -30,25 +39,29 @@ const CardBill = ({ITEMS_BILLS}:CardBillProps) => {
         >
           Cuentas
         </label>
-        <select
-          id="idBill"
-          className="form-select appearance-none block w-full px-3 py-2 text-base font-normal text-gray-500 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none mt-4"
-          onChange={handleCurrentBill}
-          value={bill.id}
-        >
-          <option disabled>
-            --Seleccionar--
-          </option>
-          {ITEMS_BILLS.map(({ id, type }, index) => (
-            <option key={index} value={id}>
-              {`Cuenta de ${type} : ${id}`}
-            </option>
-          ))}
-        </select>
-        <p className="text-darkprimary mt-6">
-          Saldo disponible: <span className="text-gray-500">{bill.id}</span>
-        </p>
-        <p className="text-4xl mt-2">{`$${bill.balance}`}</p>
+        {bill ? (
+          <>
+            <select
+              id="idBill"
+              className="form-select appearance-none block w-full px-3 py-2 text-base font-normal text-gray-500 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none mt-4"
+              onChange={handleCurrentBill}
+              value={bill.id}
+            >
+              <option disabled>--Seleccionar--</option>
+              {ITEMS_BILLS.map(({ id, type }, index) => (
+                <option key={index} value={id}>
+                  {`Cuenta de ${type} : ${id}`}
+                </option>
+              ))}
+            </select>
+            <p className="text-darkprimary mt-6">
+              Saldo disponible: <span className="text-gray-500">{bill.id}</span>
+            </p>
+            <p className="text-4xl mt-2">{`$${bill.balance}`}</p>
+          </>
+        ) : (
+          <p>No hay cuentas creadas</p>
+        )}
       </div>
     </>
   );
--- src/components/Home/Card/credit-card/index.tsx
+++ src/components/Home/Card/credit-card/index.tsx
@@ -1,65 +1,80 @@
-import SideBar from "@components/Globals/Layout/Sidebar";
-import { useState } from "react";
-import Button from "@components/Globals/Button/Button"
+import Button from "@components/Globals/Button/Button";
+import { useEffect, useState } from "react";
+
 interface CreditCard {
-  id: number,
-  number: string,
-  security_code: string,
-  expiration_date: string,
-  user: number
+  id: number;
+  number: string;
+  security_code: string;
+  expiration_date: string;
+  user: number;
 }
 interface CreditCardProps {
   ITEMS_CARDS: CreditCard[];
 }
 
-const CreditCard = ({ITEMS_CARDS}:CreditCardProps) => {
-  console.log("Credit Card",ITEMS_CARDS)
-  const [bill, setbill] = useState(ITEMS_CARDS[0]);
+const CreditCard = ({ ITEMS_CARDS }: CreditCardProps) => {
+  console.log("Credit Card", ITEMS_CARDS);
+  const [bill, setbill] = useState<any>();
   const handleCurrentBill = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const findBill = ITEMS_CARDS.find(({ id }) => e.target.value == id.toString());
+    const findBill = ITEMS_CARDS.find(
+      ({ id }) => e.target.value == id.toString()
+    );
     if (findBill) {
       setbill(findBill);
     }
   };
+  useEffect(() => {
+    if (ITEMS_CARDS) {
+      setbill(ITEMS_CARDS[0]);
+    }
+  }, [ITEMS_CARDS]);
+
   return (
     <>
       <div className="rounded-2xl w-full overflow-hidden shadow-lg p-8">
         <label
           className="text-darkprimary font-bold uppercase"
-          htmlFor="idBill"
+          htmlFor="idCard"
         >
           Tarjetas de Crédito
         </label>
-        <select
-          id="idBill"
-          className="form-select appearance-none block w-full px-3 py-2 text-base font-normal text-gray-500 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none mt-4"
-          onChange={handleCurrentBill}
-          value={bill.id}
-        >
-          <option disabled>--Seleccionar--</option>
-          {ITEMS_CARDS.map(({ id }, index) => (
-            // Change for id when not static data
-            <option key={index} value={id}>
-              {`Tarjeta : ${id}`}
-            </option>
-          ))}
-          <SideBar />
-        </select>
-        <div className="flex justify-between items-center">
-          <div>
-            <p className="text-darkprimary mt-6">
-              Numero de Tarjeta: <span className="text-gray-500">{bill.number}</span>
-            </p>
-            <p className="text-darkprimary">
-              Fecha de Expiracion: <span className="text-gray-500">{bill.expiration_date}</span>
-            </p>
-          </div>
-          <div>
-            <Button>
-              <p>Cancelar</p>
-            </Button>
-          </div>
-        </div>
+        {bill ? (
+          <>
+            <select
+              id="idCard"
+              className="form-select appearance-none block w-full px-3 py-2 text-base font-normal text-gray-500 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none mt-4"
+              onChange={handleCurrentBill}
+              value={bill.id}
+            >
+              <option disabled>--Seleccionar--</option>
+              {ITEMS_CARDS.map(({ id }, index) => (
+                // Change for id when not static data
+                <option key={index} value={id}>
+                  {`Tarjeta : ${id}`}
+                </option>
+              ))}
+            </select>
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-darkprimary mt-6">
+                  Numero de Tarjeta:{" "}
+                  <span className="text-gray-500">{bill.number}</span>
+                </p>
+                <p className="text-darkprimary">
+                  Fecha de Expiracion:{" "}
+                  <span className="text-gray-500">{bill.expiration_date}</span>
+                </p>
+              </div>
+              <div>
+                <Button>
+                  <p>Cancelar</p>
+                </Button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p> No hay tarjetas de crédito asociadas </p>
+        )}
       </div>
     </>
   );

3609ac2b1927fab371f7240d9b532574b3ab4883
Gabriela Ustariz <gabrielaustariz@gmail.com>
2022-03-02 18:46:34 -0400

    added changes

--- src/components/Globals/ErrorMessage/index.tsx
+++ src/components/Globals/ErrorMessage/index.tsx
@@ -5,21 +5,25 @@ interface ErrorProps {
 }
 
 const ErrorMessage = ({ name, error, embed = false }: ErrorProps) => {
-  const obj = (embed) ? name.split(".")[0] : name;
-  // const let : string| (embed) ? name.split(".")[1] : null;
+  const obj = embed ? name.split(".")[0] : name;
+  const obj2: string | null = embed ? name.split(".")[1] : null;
   return (
     <>
       {error && error.info && error.info.detail && error.info.detail[obj] && (
         <div className="bg-red-400 border border-red-700 w-full p-3 my-3 py-2 rounded-lg text-sm font-normal">
           <strong>Error: </strong>
-          {error.info.detail[obj].map((element: string, index: number) => (
-            <span key={index}>{element}</span>
-          ))}
+          {!obj2
+            ? error.info.detail[obj].map((element: string, index: number) => (
+                <span key={index}>{element}</span>
+              ))
+            : error.info.detail[obj][obj2].map(
+                (element: string, index: number) => (
+                  <span key={index}>{element}</span>
+                )
+              )}
         </div>
       )}
     </>
-
-    
   );
 };
 
--- src/components/PayGateWay/CreditCard/index.tsx
+++ src/components/PayGateWay/CreditCard/index.tsx
@@ -66,7 +66,6 @@ const CreditCard = ({
         <Formik
           initialValues={initialValue}
           onSubmit={(values: CreditCardForm) => {
-            alert(JSON.stringify(values, null, 2));
             handleSubmit(values);
           }}
         >
@@ -96,24 +95,40 @@ const CreditCard = ({
               type="text"
               placeholder="0000 0000 0000 0000 0000"
             />
-            <ErrorMessage name="card.number" error={messageError} embed={true} />
+            <ErrorMessage
+              name="card.number"
+              error={messageError}
+              embed={true}
+            />
             <div className="flex gap-x-2">
-              <Field
-                name="expiration_date"
-                className="shadow appearance-none border-gray-300 rounded-b py-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/2"
-                id="expiration_date"
-                type="date"
-                placeholder="dd/mm"
-              />
-              <ErrorMessage name="card.expiration_date" error={messageError} embed={true} />
-              <Field
-                name="security_code"
-                className="shadow appearance-none border-gray-300 rounded-b py-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-1/2"
-                id="security_code"
-                type="text"
-                placeholder="CVC"
-              />
-              <ErrorMessage name="card.security_code" error={messageError} embed={true} />
+              <div className="w-1/2">
+                <Field
+                  name="expiration_date"
+                  className="shadow appearance-none border-gray-300 rounded-b py-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                  id="expiration_date"
+                  type="date"
+                  placeholder="dd/mm"
+                />
+                <ErrorMessage
+                  name="card.expiration_date"
+                  error={messageError}
+                  embed={true}
+                />
+              </div>
+              <div className="w-1/2">
+                <Field
+                  name="security_code"
+                  className="shadow appearance-none border-gray-300 rounded-b py-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                  id="security_code"
+                  type="text"
+                  placeholder="CVC"
+                />
+                <ErrorMessage
+                  name="card.security_code"
+                  error={messageError}
+                  embed={true}
+                />
+              </div>
             </div>
             {sucessTransaction && (
               <div

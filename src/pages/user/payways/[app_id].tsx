b060485a92dd953154ccaed7dfd61bf85e322e60
vixx_ <vittorio.adesso@gmail.com>
2022-03-01 20:32:17 -0400

    chore: swap keys

--- src/pages/user/payways/[app_id].tsx
+++ src/pages/user/payways/[app_id].tsx
@@ -44,15 +44,14 @@ const PasarelaOptions: NextPage = () => {
         </div>
         <>
           <label className="block text-sm xl:text-md pt-5 font-bold mb-2 text-dark">
+            Clave pública
+          </label>
+          <p>{currentkeys?.public}</p>
+          <label className="block text-sm xl:text-md pt-5 font-bold mb-2 text-dark">
             Clave privada
           </label>
 
           <p>{currentkeys?.private}</p>
-
-          <label className="block text-sm xl:text-md pt-5 font-bold mb-2 text-dark">
-            Clave pública
-          </label>
-          <p>{currentkeys?.public}</p>
         </>
 
         <div className="mt-8">
@@ -78,8 +77,8 @@ const PasarelaOptions: NextPage = () => {
           )}
           <Button
             className=" w-full md:w-60 bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            onClick={() => {
-              setCurrentKeys(renewKey(appId));
+            onClick={async () => {
+              setCurrentKeys(await renewKey(appId));
               setWarningAlert(true);
             }}
           >

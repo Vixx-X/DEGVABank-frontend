3ee076a238b60c0a70897d1b4466abc572594c00
EduhuSua <Eduardosuarez.ucv@gmail.com>
2022-03-09 21:21:01 -0400

    no mergear

--- src/components/Globals/ErrorMessage/index.tsx
+++ src/components/Globals/ErrorMessage/index.tsx
@@ -7,16 +7,17 @@ interface ErrorProps {
 const ErrorMessage = ({ name, error, embed = false }: ErrorProps) => {
   const obj = embed ? name.split(".")[0] : name;
   const obj2: string | null = embed ? name.split(".")[1] : null;
+  console.log("Name",obj)
   return (
     <>
-      {error && error.info && error.info[obj] && (
+      {error && error.info && error.info && error.info.detail && error.info.detail[obj] &&(
         <div className="bg-red-400 border border-red-700 w-full p-3 my-3 py-2 rounded-lg text-sm font-normal">
           <strong>Error: </strong>
           {!obj2
-            ? error.info[obj].map((element: string, index: number) => (
+            ? error.info.detail[obj].map((element: string, index: number) => (
                 <span key={index}>{element}</span>
               ))
-            : error.info[obj][obj2].map(
+            : error.info.detail[obj][obj2].map(
                 (element: string, index: number) => (
                   <span key={index}>{element}</span>
                 )
